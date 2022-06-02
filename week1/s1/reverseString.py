91f1e27716124355e16acf9d1df25e53739c2640
QuangHy-gh <quanghynguyen1@gmail.com>
2022-06-01 23:50:55 -0500

    initial commit section 1 problem 1

---
+++ week1/s1/reverseString.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> List[str]:
+
+        l_ptr = 0
+        r_ptr = len(s) - 1
+
+        while l_ptr < r_ptr:
+            temp = s[l_ptr]
+            s[l_ptr] = s[r_ptr]
+            s[r_ptr] = temp
+            l_ptr += 1
+            r_ptr -= 1
+
+        return s
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    str = "yendyS ,yaW yballaW 24"
+    list_str = list(str)
+    print("".join(sol.reverseString(list_str)))
